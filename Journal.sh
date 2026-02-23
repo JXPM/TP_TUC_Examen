273b2c6ef38bb85aed4c56d71c27b9447c3d2860
JXPM <bilejohan04@gmail.com>
2026-02-23 11:15:49 +0100

    first commit

--- Journal.sh
+++ Journal.sh
@@ -3,6 +3,6 @@ git init
 git branch -M main
 git add .
 git commit -m "first commit"
-gh repo create bar-saas-backend --private
+gh repo create TP_TUC_Exam --private
 git remote add origin https://github.com/JXPM/TP_TUC_Exam.git
 git push --set-upstream origin main
